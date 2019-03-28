412cda9bd0f0f1a3b2556196d576ab0a9ffb49b7
Michael Hu <pxmyhdev@gmail.com>
2019-03-27 22:04:59 -0400

    demo and tested working on real system up to select csv option for export

--- config.js
+++ config.js
@@ -10,11 +10,12 @@ console.log("Getting INTERVAL: " + interval);
 
 module.exports = {
   URL: "https://10.23.77.169/e911-lam-gui/init.do",
-  USERNAME: "admin",
-  PASSWORD: "admin",
+  USERNAME: "admin", // Action: change this
+  PASSWORD: "admin", // Action: change this
   INTERVAL: "5", // minutes
   DATE_SLECTOR: "#searchCallTimestampInput",
   EXPORT_SELECTOR:
     "#dataTable_wrapper > div.fg-toolbar.ui-toolbar.ui-widget-header.ui-helper-clearfix.ui-corner-tl.ui-corner-tr > div > a.dt-button.ui-button.ui-state-default.ui-button-text-only.buttons-collection > span",
-  CSV_SELECTOR: "body > div.dt-button-collection > a:nth-child(2) > span"
+  CSV_SELECTOR: "body > div.dt-button-collection > a:nth-child(2) > span",
+  EXCEL_SELECTOR: "body > div.dt-button-collection > a:nth-child(1) > span"
 };
--- config_test.js
+++ config_test.js
@@ -10,8 +10,6 @@ console.log("Getting INTERVAL: " + interval);
 
 module.exports = {
   // URL: "http://localhost:3000/",
-  // URL: "http://www.latabledarc.com/trianon-assiette-a-pain.html",
-  // URL: "https://112.126.83.219/",
   URL:
     "https://www.sec.gov/cgi-bin/browse-edgar?CIK=AAPL&action=getcompany&owner=exclude",
   USERNAME: "simon",
@@ -20,5 +18,6 @@ module.exports = {
   DATE_SLECTOR: "#prior_to",
   EXPORT_SELECTOR:
     "#dataTable_wrapper > div.fg - toolbar.ui - toolbar.ui - widget - header.ui - helper - clearfix.ui - corner - tl.ui - corner - tr > div > a.dt - button.ui - button.ui - state -default.ui - button - text - only.buttons - collection > span",
-  CSV_SELECTOR: "body > div.dt-button-collection > a:nth-child(2) > span"
+  CSV_SELECTOR: "body > div.dt-button-collection > a:nth-child(2) > span",
+  EXCEL_SELECTOR: "body > div.dt-button-collection > a:nth-child(1) > span"
 };
